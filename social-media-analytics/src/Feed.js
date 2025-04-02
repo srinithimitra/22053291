88d3a0e36b9d234f0b06eb342765811cb731d774
Srinithi Mitra <22053291@kiit.ac.in>
2025-04-02 18:58:06 +0530

    Update Feed.js

--- social-media-analytics/src/Feed.js
+++ social-media-analytics/src/Feed.js
@@ -1,1 +1,23 @@
+// social-media-analytics/src/Feed.js
+import React from 'react';
 
+const Feed = ({posts, users, comments})=>{
+  if (!posts) return <div>Loading...</div>;
+
+  const sortedPosts = [...posts].sort((a,b)=>b.id-a.id);
+
+  return (
+    <div>
+      <h2>Feed</h2>
+      <ul>
+        {sortedPosts.map((post)=>(
+          <li key = {post.id}>
+            {users[post.userid]}: {post.content} ({comments[post.id] ? comments[post.id].length : 0} comments)
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Feed;
